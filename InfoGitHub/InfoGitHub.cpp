f4e0943f632e97a78e871408bb0ecd3d55b08536
catulescumarianicola <catulescumarianicola@gmail.com>
2026-01-01 18:36:05 +0200

    Add matrix region sum calculations and max finder
    Replaces the Hello World example with logic to read an n x n matrix, compute sums for four specific regions (Sn, Ss, Sv, Se), print them, and output the maximum among them. This provides a basis for analyzing matrix regions based on their indices.

--- InfoGitHub/InfoGitHub.cpp
+++ InfoGitHub/InfoGitHub.cpp
@@ -1,20 +1,37 @@
-// InfoGitHub.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 
+using namespace std;
+int a[100][100],n,i,j;
 int main()
 {
-    std::cout << "Hello World!\n";
+    int Sn=0, Ss=0, Sv=0, Se=0, Smax=0;
+    cin>>n;
+    for (i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            cin>>a[i][j];
+    for (i=0;i<n;i++)
+        for(j=0;j<n;j++){
+            if(i<j && i+j<=n-2)
+                Sn+=a[i][j];
+            if (i>j && i+j>=n)
+                Ss+=a[i][j];
+            if (i>j && i+j<=n-2)
+                Sv+=a[i][j];
+            if (i<j && i+j>=n)
+                Se+=a[i][j];
+        }
+    cout << Sn<<" "<< Ss<<" "<<Sv<<" "<<Se<< endl;
+
+    int v[5];
+    v[0]=Sn;
+    v[1]=Ss;
+    v[2]=Sv;
+    v[3]=Se;
+
+    for(i=0; i<4; i++)
+        if (v[i]>Smax)
+            Smax=v[i];
+    cout<< Smax;
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
