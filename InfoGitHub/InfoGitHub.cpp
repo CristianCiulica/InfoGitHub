6e31cb1fe3222b22d5a0feacc5509d928118a096
catulescumarianicola <catulescumarianicola@gmail.com>
2026-01-01 18:56:50 +0200

    Refactor matrix sum logic and add GCD calculation
    Simplified the matrix region sum calculations to only compute sums above and below the main diagonal. Introduced a cmmdc function to compute the GCD of these sums and output the result, removing previous logic for quadrant sums and maximum value.

--- InfoGitHub/InfoGitHub.cpp
+++ InfoGitHub/InfoGitHub.cpp
@@ -1,37 +1,36 @@
-
 #include <iostream>
 
 using namespace std;
-int a[100][100],n,i,j;
+int a[100][100],n ,i ,j;
+
+int cmmdc(int a,int b){
+    while (b!=0){
+    int r;
+    r=a%b;
+    a=b;
+    b=r;
+    }
+    return a;
+}
+
 int main()
 {
-    int Sn=0, Ss=0, Sv=0, Se=0, Smax=0;
-    cin>>n;
-    for (i=0;i<n;i++)
-        for(j=0;j<n;j++)
-            cin>>a[i][j];
-    for (i=0;i<n;i++)
-        for(j=0;j<n;j++){
-            if(i<j && i+j<=n-2)
-                Sn+=a[i][j];
-            if (i>j && i+j>=n)
-                Ss+=a[i][j];
-            if (i>j && i+j<=n-2)
-                Sv+=a[i][j];
-            if (i<j && i+j>=n)
-                Se+=a[i][j];
+  cin>> n;
+  for (i=0; i<n; i++)
+    for(j=0; j<n;j++)
+        cin>> a[i][j];
+
+  int Sdeasupra=0, Ssub=0;
+
+    for (i=0; i<n; i++)
+        for(j=0; j<n;j++){
+            if (i<j)
+                Sdeasupra+=a[i][j];
+            if (i>j)
+                Ssub+=a[i][j];
         }
-    cout << Sn<<" "<< Ss<<" "<<Sv<<" "<<Se<< endl;
+    cout << cmmdc(Sdeasupra,Ssub);
 
-    int v[5];
-    v[0]=Sn;
-    v[1]=Ss;
-    v[2]=Sv;
-    v[3]=Se;
 
-    for(i=0; i<4; i++)
-        if (v[i]>Smax)
-            Smax=v[i];
-    cout<< Smax;
     return 0;
 }
