9c90d5afe1e2c52f05b6323d4b646e27604605fe
Cristian <cristian.ciulica@student.unitbv.ro>
2026-01-20 11:50:04 +0200

    Problema fisiere

--- InfoGitHub/InfoGitHub.cpp
+++ InfoGitHub/InfoGitHub.cpp
@@ -1,19 +1,29 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
-int a[20][20], i, j, sMax, n, m;
+ifstream fin("in.txt");
+int x, a = -1, b = -1;
 int main() {
-	cin >> n >> m;
-	for (i = 0; i < n; i++)
-		for (j = 0; j < m; j++)
-			cin >> a[i][j];
-	for (i = 0; i < n - 1; i++) {
-		for (j = 0; j < m - 1; j++) {
-			int sumaCurenta = 0;
-			sumaCurenta = a[i][j + 1] + a[i + 1][j] + a[i + 1][j + 1] + a[i][j];
-			if (sumaCurenta > sMax)
-				sMax = sumaCurenta;
+	while (fin >> x) {
+		if (x % 2 == 0) {
+			if (a == -1 && b == -1)
+				a = x;
+			else if (a != -1 && b == -1)
+				b = x;
+			else if (a != -1 && b != -1) {
+				a = b;
+				b = x;
+			}
 		}
 	}
-	cout << sMax;
+	// 1 4 6 5 7 8 5 8 
+	//   a b
+		// a     b
+	if (a == -1 || b == -1)
+		cout << "NU EXISTA\n";
+	else
+		cout << a << " " << b;
+	fin.close();
+	
 	return 0;
 }
